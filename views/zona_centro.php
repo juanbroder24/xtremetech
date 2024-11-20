d7ba71efe2714f81e0b0664b504b722c0b9d9086
Juan24 <93536474+juanbroder24@users.noreply.github.com>
2024-11-20 17:11:02 -0300

    style_update

--- views/zona_centro.php
+++ views/zona_centro.php
@@ -22,93 +22,13 @@ if (isset($_POST['zona'])) {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-   <link rel="shortcut icon" type="image/x-icon" href="../assets/IMG/favicon-xtremetech.ico">
-  <title>XtremeTech</title>
-  <link rel="stylesheet" href="../assets/css/bootstrap.css">
-   <style>
-    /* Estilo para el fondo de la página */
-    body {
-      background-image: url('../assets/IMG/centro.jpg');
-      background-size: cover; /* Ajusta la imagen para que cubra toda el área */
-      background-repeat: no-repeat; /* Evita la repetición de la imagen */
-      background-position: center center; /* Centra la imagen */
-      background-attachment: fixed; /* Evita el desplazamiento de la imagen */
-      height: 100vh; /* Establece la altura de la página al 100% del viewport */
-      margin: 0; /* Elimina el margen por defecto del body */
-      padding: 0; /* Elimina el padding por defecto del body */
-      background-color: rgba(255, 255, 255, 0.8); /* Agrega una capa de color semitransparente */
-      filter: hue-rotate(20deg) saturate(150%); /* Aplica filtros de color */
-    }
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/IMG/favicon-xtremetech.ico">
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
 
-
-
-    /* Estilo para los botones */
-
-    .btn-danger {
-
-      background-color: #D9534F;
-
-      border-color: #D9534F;
-
-    }
-
-
-
-    .btn-danger:hover {
-
-      background-color: #C9302C;
-
-      border-color: #B92C28;
-
-    }
-
-
-
-    .btn-danger:focus, .btn-danger.focus {
-
-      box-shadow: 0 0 0 0.25rem rgba(217, 83, 79, 0.5);
-
-    }
-
-
-
-    .btn-danger.disabled, .btn-danger:disabled {
-
-      background-color: #D9534F;
-
-      border-color: #D9534F;
-
-    }
-
-
-
-    .btn-danger:not(:disabled):not(.disabled):active, .btn-danger:not(:disabled):not(.disabled).active,
-
-    .show > .btn-danger.dropdown-toggle {
-
-      background-color: #B92C28;
-
-      border-color: #AC2925;
-
-    }
-
-    .btn-danger:not(:disabled):not(.disabled):active:focus, .btn-danger:not(:disabled):not(.disabled).active:focus,
-
-    .show > .btn-danger.dropdown-toggle:focus {
-
-      box-shadow: 0 0 0 0.25rem rgba(217, 83, 79, 0.5);
-
-    }
-	.dropdown-item:hover {
-      background-color: #D9534F; /* Color de fondo del botón en hover */
-      color: white; /* Color del texto del botón en hover */
-    }
-
-  </style>
 </head>
 <body>
 
